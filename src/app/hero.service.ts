7eaabe4251045a5e0b231cced7786d6ea74ade77
Jose Posada <josedanielpm@gmail.com>
2018-11-15 22:35:55 -0500

    6.3 HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,9 +5,14 @@ import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
   constructor(
@@ -40,6 +45,14 @@ export class HeroService {
     );
   }
 
+/** PUT: update the hero on the server */
+updateHero (hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
+
   /**
  * Handle Http operation that failed.
  * Let the app continue.
@@ -48,13 +61,13 @@ export class HeroService {
  */
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-  
+
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-  
+
       // TODO: better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
-  
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
